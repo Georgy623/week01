2049c09c017b61b3129659eb23212ada150f34ab
Georgy623 <90360245+Georgy623@users.noreply.github.com>
2023-02-09 09:07:22 -0600

    Create 02.2_Variable_Declaration_and_Initialization .kt

---
+++ 02.2_Variable_Declaration_and_Initialization .kt
@@ -0,0 +1,19 @@
+// Georgy Thekkanal
+// 02.2 Variable Declaration
+// January 12, 2023
+fun main() {
+    
+    var myAge=19
+    var averageAge=24.5
+    
+    var testNumber = 5
+    var evenOdd = testNumber % 2
+    
+    var answer = 0
+        answer += (1+10)*10*1000
+        
+    	
+    
+    println(answer)
+  
+} 
