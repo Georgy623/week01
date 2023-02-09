62827dc22fc3ad405fb11858142b8847285ed6e3
Georgy623 <90360245+Georgy623@users.noreply.github.com>
2023-02-09 09:30:50 -0600

    Create marketing.kt

---
+++ marketing.kt
@@ -0,0 +1,16 @@
+// Georgy Thekkanal
+// 02.3 Advertising Conditional
+// January 12, 2023
+fun main() {
+    
+
+var FriendCount: Int=700
+if (FriendCount > 500) {
+    println("Adopt a Dog Today!")
+} else {
+    println("Buy Cat Food At 20% Off")
+    
+}
+    
+    
+}
