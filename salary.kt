9878dbfda8a462f4e2033ce5b2e2a95a319ee70b
Georgy623 <90360245+Georgy623@users.noreply.github.com>
2023-02-09 09:46:46 -0600

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,11 @@
+// Georgy Thekkanal
+// 02.4 Name, Age and Annual Income
+// January 12, 2023
+fun main() {
+    var name="Name"
+    var age=19
+    var annualPay= 200000
+    
+    println("My name is " + name+", my age is "+ age+ " and I hope to earn " +annualPay+" per year.")
+    
+}
