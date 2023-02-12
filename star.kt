6e1027bea803cf0f248490e06b7c09de24d92276
Georgy623 <90360245+Georgy623@users.noreply.github.com>
2023-02-12 14:50:43 -0600

    Create star.kt

---
+++ star.kt
@@ -0,0 +1,16 @@
+// Georgy Thekkanal
+// 02.5 Star Pattern
+// January 12, 2023
+fun main() {
+    
+    
+    println("...*")
+    println("..***")
+    println(".*****")
+    println("*******")
+    println(".*****")
+    println("..***")
+    println("...*")
+
+    
+}
