46cd25266024603f19c8df577d159810fabb237f
Georgy623 <90360245+Georgy623@users.noreply.github.com>
2023-02-12 14:55:49 -0600

    Create salesPrediction.kt

---
+++ salesPrediction.kt
@@ -0,0 +1,10 @@
+// Georgy Thekkanal
+// 02.6 Sales Prediction
+// January 12, 2023
+fun main() {
+    var eastCoastSales=.62
+    var totalRevenue=4600000
+    var salesPredediction=totalRevenue*eastCoastSales
+    println("$"+salesPredediction)
+    
+}
